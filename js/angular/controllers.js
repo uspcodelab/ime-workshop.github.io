1cf88bd6f114bab55204ef61207262429b37b4bb
akaFTS <gustavo84171@hotmail.com>
2016-04-21 00:02:31 -0300

    Estrutura que exibe lista de eventos funcionando.
    Falta exibir detalhes de cada evento num modal.

--- js/angular/controllers.js
+++ js/angular/controllers.js
@@ -2,8 +2,59 @@ var app = angular.module("workSite", []);
 
 //CONTROLLERS
 
-app.controller("listaCtrl", function($scope){
-    $scope.blah = "bleh";
+app.controller("listaCtrl", function($scope, fetchEventos){
+    fetchEventos.fetchLista().then(function(data){
+        $scope.eventos = data;
+    });
+
+    $scope.getColor = function(classe) {
+        switch(classe) {
+            case "nubank":
+                return "purple";
+            case "hardware":
+                return "blue";
+            case "gamedev":
+                return "orange";
+            default:
+                return "def"; //classe falsa, não existe
+        }
+    }
+
+    $scope.getBackColor = function(classe) {
+        return $scope.getColor(classe)+"-back";
+    }
 });
 
 //SERVICES
+
+app.service("fetchEventos", function($http, $q){
+    this.fetchLista = function() {
+        var deferred = $q.defer();
+        $http.get("eventos.json").then(function(data){
+            var lista = [];
+            for(var x in data.data) {
+                var evento = data.data[x];
+                if(moment(evento.data, "DD/MM/YYYY").isBefore(moment(), "day")) {
+                    lista.push(evento);
+                }
+            }
+            deferred.resolve(lista);
+        });
+        return deferred.promise;
+    }
+
+    this.fetchFuturos = function() {
+        var deferred = $q.defer();
+        $http.get("eventos.json").then(function(data){
+            var lista = [];
+            for(var x in data.data) {
+                var evento = data.data[x];
+                if(moment(evento.data, "DD/MM/YYYY").isSameOrAfter(moment(), "day")) {
+                    lista.push(evento);
+                }
+            }
+            deferred.resolve(lista);
+        });
+        return deferred.promise;
+    }
+});
