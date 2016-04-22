7dec8fbb10708ef4f5241896fefe6d8055eeb105
akaFTS <gustavo84171@hotmail.com>
2016-04-22 18:44:51 -0300

    Adicionado layout base do calendário

--- js/angular/controllers.js
+++ js/angular/controllers.js
@@ -1,28 +1,39 @@
-var app = angular.module("workSite", []);
+var app = angular.module("workSite", ["flexcalendar"]);
 
 //CONTROLLERS
 
-app.controller("listaCtrl", function($scope, fetchEventos){
+app.controller("proxCtrl", function($scope, fetchEventos, colorService){
+    $scope.options = {
+        minDate: "2016-01-01",
+        maxDate: "2019-12-31",
+        eventClick: function(date) { // called before dateClick and only if clicked day has events
+          console.log(date);
+        },
+        dateClick: function(date) { // called every time a day is clicked
+          console.log(date);
+        }
+    };
+    fetchEventos.fetchTudo().then(function(data){
+        var events = [];
+        // o flex-calendar exige preparação: um atributo chamado date, padrão YYYY-MM-DD
+        // e ainda por cima tem um bug que as datas aparecem um dia antes do correto
+        for(var x in data) {
+            var evento = data[x];
+            evento.date = moment(evento.data, "DD/MM/YYYY").add(1, "days").format("YYYY-MM-DD");
+            evento.eventClass = colorService.getEventColor(evento.classe);
+            events.push(evento);
+        }
+        $scope.events = events;
+    });
+});
+
+app.controller("listaCtrl", function($scope, fetchEventos, colorService){
     fetchEventos.fetchLista().then(function(data){
-        $scope.eventos = data;
+        $scope.events = data;
     });
 
-    $scope.getColor = function(classe) {
-        switch(classe) {
-            case "nubank":
-                return "purple";
-            case "hardware":
-                return "blue";
-            case "gamedev":
-                return "orange";
-            default:
-                return "def"; //classe falsa, não existe
-        }
-    }
-
-    $scope.getBackColor = function(classe) {
-        return $scope.getColor(classe)+"-back";
-    }
+    $scope.getColor = colorService.getColor;
+    $scope.getBackColor = colorService.getBackColor;
 
     $scope.makeModal = function(evento) {
         $scope.curvento = evento;
@@ -60,5 +71,36 @@ app.service("fetchEventos", function($http, $q){
             deferred.resolve(lista);
         });
         return deferred.promise;
+    }
+
+    this.fetchTudo = function() {
+        var deferred = $q.defer();
+        $http.get("eventos.json").then(function(data){
+            deferred.resolve(data.data);
+        });
+        return deferred.promise;
+    }
+});
+
+app.service("colorService", function(){
+    this.getColor = function(classe) {
+        switch(classe) {
+            case "nubank":
+                return "purple";
+            case "hardware":
+                return "teal";
+            case "gamedev":
+                return "orange";
+            default:
+                return "def"; //classe falsa, não existe
+        }
+    }
+
+    this.getBackColor = function(classe) {
+        return this.getColor(classe)+"-back";
+    }
+
+    this.getEventColor = function(classe) {
+        return this.getColor(classe)+"-event";
     }
 });
